2b73801491bb34543fd4f2e2493289e22e9a2c2e
Cedric <cedrichartskeerl@gmail.com>
2021-06-30 15:56:46 +0200

    add alerts (buggy for now) and some refactoring

--- stop_hoarsing_around.rb
+++ stop_hoarsing_around.rb
@@ -3,13 +3,25 @@ require 'rb-inotify'
 require 'pry'
 require 'open3'
 
+# Tweakable variables:
+test_group_size					   = 5
+max_tense_percentage			 = 50
+upper_confidence_threshold = 0.85
+lower_confidence_threshold = 0.2
+
 wav_queue 					= []
 results							= []
 spectrogram_queue 	= []
-tense_percentage		= 50
-log_manager					= Mutex.new
+$log_manager					= Mutex.new
 wav_queue_manager 	= Mutex.new
 spectrogram_queue_manager = Mutex.new
+
+def log_event(message)
+	timestamp = Time.now.strftime("%Y-%m-%d %k:%M:%S.%L")
+	$log_manager.synchronize {
+		File.open('log.txt', 'a') { |f| f.puts "[#{timestamp}] #{message}" }
+	}
+end
 
 folders = ["recordings", "processing", "spectrograms"]
 folders.each do |dir|
@@ -28,12 +40,8 @@ wav_queue_thread = Thread.new {
 		filename = event.absolute_name.delete_prefix("./recordings/")
 
     # puts "File created: #{filename}"
-  	log_manager.synchronize {
-  		timestamp = Time.now.strftime("%Y-%m-%d %k:%M:%S.%L")
-  		File.open('log.txt', 'a') { |f|
-  		  f.puts "[#{timestamp}] File created: #{filename}"
-  		}
-  	}
+    message = "File created: #{filename}"
+  	log_event(message)
 
   	wav_queue_manager.synchronize {
 	    wav_queue.unshift(filename)
@@ -48,17 +56,13 @@ wav_processing_thread = Thread.new {
 		sleep 0.05
 		wav_queue_manager.synchronize {
 			if wav_queue.length > 15
-				while wav_queue.length > 2
+				while wav_queue.length > 5
 					split_file = wav_queue.pop
-					timestamp  = Time.now.strftime("%Y-%m-%d %k:%M:%S.%L")
 
 					# puts "Wav queue full: dropping #{split_file}"
 
-		    	log_manager.synchronize {
-						File.open('log.txt', 'a') { |f|
-						  f.puts "[#{timestamp}] Wav queue full: dropping #{split_file}"
-						}
-					}
+					message = "Wav queue full: dropping #{split_file}"
+			  	log_event(message)
 
 					FileUtils.rm "recordings/#{split_file}"
 				end
@@ -73,12 +77,11 @@ wav_processing_thread = Thread.new {
 				amplitude = `sox processing/#{split_file} -n stat 2>&1 | grep "Maximum amplitude"`.scan(/\d+\.\d+/).flatten.first.to_f * 1000.to_i
 		  	# puts amplitude
 
-		 		# Remove silent splits
   	  	if amplitude < 5
   	  		# puts "Low amplitude (#{amplitude}): removing #{normalized_split}"
   		  	FileUtils.rm "processing/#{split_file}"
   		  else
-  		  	 # Normalize remaining splits
+  		  	# Normalize remaining splits
 			  	`sox --norm=-3 processing/#{split_file} processing/#{normalized_split}`
 			  	FileUtils.rm "processing/#{split_file}"
 
@@ -113,12 +116,9 @@ Open3.popen3(cmd) do |stdin, stdout, _stderr, wait_thr|
 					spectrogram = spectrogram_queue.pop
 
 					# puts "Spectrogram queue full: dropping #{spectrogram}"
-					timestamp = Time.now.strftime("%Y-%m-%d %k:%M:%S.%L")
-		    	log_manager.synchronize {
-						File.open('log.txt', 'a') { |f|
-						  f.puts "[#{timestamp}] Spectrogram queue full: dropping #{spectrogram}"
-						}
-					}
+					message = "Spectrogram queue full: dropping #{spectrogram}"
+			  	log_event(message)
+
 					FileUtils.rm "spectrograms/#{spectrogram}"	
 				end
 			elsif spectrogram_queue.length > 0
@@ -130,7 +130,7 @@ Open3.popen3(cmd) do |stdin, stdout, _stderr, wait_thr|
 
 				evaluator_output = stdout.gets.strip.split("\r").last.split(",")
 
-				# p "#{spectrogram}: #{evaluator_output}"
+				# p "Spectrogram #{spectrogram}: #{evaluator_output}"
 
 				result = evaluator_output[0]
 				confidence_relaxed = evaluator_output[1].to_f
@@ -138,30 +138,39 @@ Open3.popen3(cmd) do |stdin, stdout, _stderr, wait_thr|
 
 				FileUtils.rm "spectrograms/#{spectrogram}"
 
-				if result == "relaxed" && confidence_relaxed > 0.85 && confidence_tense < 0.2
+				if result == "relaxed" && confidence_relaxed > upper_confidence_threshold && confidence_tense < lower_confidence_threshold
 					results << "relaxed"
 					puts "Confident relaxed spectrogram found: #{spectrogram} - relaxed: #{confidence_relaxed}, tense: #{confidence_tense}"
-				elsif result == "tense" && confidence_tense > 0.85 && confidence_relaxed < 0.2
+				elsif result == "tense" && confidence_tense > upper_confidence_threshold && confidence_relaxed < lower_confidence_threshold
 					results << "tense"
 					puts "Confident tense spectrogram found: #{spectrogram} - tense #{confidence_tense}, relaxed: #{confidence_relaxed}"
 				end
 
-				if results.length >= 100
-					if results.length > 100
-						results.shift(results.length - 10)
+				if results.length >= test_group_size
+					if results.length > test_group_size
+						results.shift(results.length - test_group_size)
 					end
 
-					total_results = results.length.to_f
-					tense_results = results.count("tense")
-					final_result  = (tense_results / total_results).truncate(2) * 100
+					total_results						  = results.length.to_f
+					number_of_tense_results   = results.count("tense")
+					current_tense_percentage  = (number_of_tense_results / total_results).truncate(2) * 100
 
-					puts "final result = #{final_result}"
+					# puts "Strained spectrogram percentage: #{current_tense_percentage}"
 
-					# TODO: implement xmessage pop-up
-					if final_result >= tense_percentage
-						puts "Omg straining!"
-					else
-						puts "No straining!"
+					if current_tense_percentage >= max_tense_percentage && (!@warning_given || Time.now.to_i - @last_warning >= 5)
+				  	puts "This is too tense for me!"
+
+						message = "Warning sent, #{current_tense_percentage}\% of last #{test_group_size} spectrograms are tense."
+				  	log_event(message)
+
+						alert_pid = Process.fork do
+							`paplay --volume 30000 ./sounds/notify.wav`
+							`xmessage -center -timeout 3 You are straining your voice! Take a break.`
+						end
+				    Process.detach(alert_pid)
+
+						@warning_given = true
+						@last_warning  = Time.now.to_i
 					end
 				end
 			end
